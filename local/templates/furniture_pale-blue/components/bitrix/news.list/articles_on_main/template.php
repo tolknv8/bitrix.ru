224a28153c97984b5f6c29b457674b89018554e6
AlexeyVers <juk50075@gmail.com>
2023-04-24 09:34:31 +0300

    106254 | исправил код согласно задаче
    - зачитил лишний не нужный код
    - сделал вывод переменных в коротком стиле

--- local/templates/furniture_pale-blue/components/bitrix/news.list/articles_on_main/template.php
+++ local/templates/furniture_pale-blue/components/bitrix/news.list/articles_on_main/template.php
@@ -16,19 +16,9 @@ $this->setFrameMode(true);
 ?>
 <div class="news-list">
 
-    <?php foreach ($arResult["ITEMS"] as $arItem): ?>
-
-<!--    --><?php
-//        echo '<pre>';
-//        var_dump($arItem);
-//        echo '</pre>';
-//        echo '<pre>';
-//        var_dump($arItem['DETAIL_TEXT']);
-//        echo '</pre>';
-//        die;
-//    ?>
-    <?php if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-            <a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br/>
+    <?php foreach ($arResult['ITEMS'] as $arItem):
+        if(!$arParams['HIDE_LINK_WHEN_NO_DETAIL'] || ($arItem['DETAIL_TEXT'] && $arResult['USER_HAVE_ACCESS'])):?>
+            <a href="<?echo $arItem['DETAIL_PAGE_URL']?>"><b><?echo $arItem['NAME']?></b></a><br/>
         <?endif;?>
     <div class="one_news">
         <img
@@ -41,24 +31,19 @@ $this->setFrameMode(true);
             class="news_img"
         />
         <div class="news_container">
-            <?php echo $arItem["PREVIEW_TEXT"]?>
+            <?echo $arItem['PREVIEW_TEXT']?>
             <p>
                 <?php
-                    $name = explode(" ",$arItem["CREATED_USER_NAME"], 2);
+                    $name = explode(" ",$arItem['CREATED_USER_NAME'], 2);
                     echo $name[1];
                 ?>
             </p>
         </div>
-        <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-            <a href="<?echo $arItem["DETAIL_PAGE_URL"]?>" style="visibility: hidden"><b><?echo $arItem["NAME"]?></b></a><br />
+        <?if(!$arParams['HIDE_LINK_WHEN_NO_DETAIL'] || ($arItem['DETAIL_TEXT'] && $arResult['USER_HAVE_ACCESS'])):?>
+            <a href="<?echo $arItem['DETAIL_PAGE_URL']?>" style="visibility: hidden"><b><?echo $arItem['NAME']?></b></a><br />
         <?endif;?>
 
     </div>
 
-    <?php endforeach; ?>
+    <?endforeach; ?>
 </div>
-<style>
-    .one_news{display: flex; align-items: center; margin-top: 10px; margin-bottom: 10px;}
-    .one_news .news_img{/*flex-direction: row;*/}
-    .one_news .news_container{/*flex-direction: row;*/ margin-left: 15px}
-</style>
