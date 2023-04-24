09e4fa83ed6b73c8de88023df9618a4f3ac4b6f7
d.kosirev <tolkinv8@gmail.com>
2023-04-24 12:01:26 +0300

    106254 | рефакторинг кода
    - убрал двойные ковычки
    - удалил ненужные комментарии
    - изменил вывод автора статьи

--- .top.menu.php
+++ .top.menu.php
@@ -34,6 +34,13 @@ $aMenuLinks = Array(
 		Array(), 
 		Array(), 
 		"" 
+	),
+	Array(
+		"Каталог", 
+		"catalog/", 
+		Array(), 
+		Array(), 
+		"" 
 	)
 );
 ?>
--- articles/index.php
+++ articles/index.php
@@ -1,5 +1,5 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Статьи");
-require "../local/articlesList.php"
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+$APPLICATION->SetTitle('Статьи');
+require '../local/articlesList.php'
+?><? require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
