96867896bd9e87d7b45915a032db4df4d8ddfe80
d.kosirev <tolkinv8@gmail.com>
2023-04-26 11:37:31 +0300

    106333 | базовая структура компонента

--- index.php
+++ index.php
@@ -3,6 +3,30 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
 
 ?>
 
+<?php
+$APPLICATION->IncludeComponent(
+    'bitrix:catalog.section.list', 'catalog_main_sections',
+    [
+        'VIEW_MODE'           => 'TEXT',
+        'SHOW_PARENT_NAME'    => 'Y',
+        'IBLOCK_TYPE'         => 'catalog',
+        'IBLOCK_ID'           => 6,
+        'SECTION_ID'          => $_REQUEST['SECTION_ID'],
+        'SECTION_CODE'        => '',
+        'SECTION_URL'         => '',
+        'COUNT_ELEMENTS'      => 'Y',
+        'TOP_DEPTH'           => '2',
+        'SECTION_FIELDS'      => ['PREVIEW_TEXT', 'PREVIEW_IMAGE'],
+        'SECTION_USER_FIELDS' => '',
+        'ADD_SECTIONS_CHAIN'  => 'Y',
+        'CACHE_TYPE'          => 'Y',
+        'CACHE_TIME'          => '36000000',
+        'CACHE_NOTES'         => '',
+        'CACHE_GROUPS'        => 'Y',
+    ]
+);
+?>
+
 
     <p>
         Наша компания существует на Российском рынке с 1993 года. За это время «Мебельная компания» прошла большой путь
