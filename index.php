f6c7818cc63998033fccdd766d4816122900fa9c
d.kosirev <tolkinv8@gmail.com>
2023-04-28 11:49:42 +0300

    106333 | форматирование кода и структуры

--- index.php
+++ index.php
@@ -1,59 +1,29 @@
-<link rel="stylesheet" href="/local/templates/furniture_pale-blue/components/bitrix/catalog.section.list/catalog_main_sections/owlcarousel/owl.carousel.min.css">
+<link rel="stylesheet"
+      href="/local/include/owlcarousel/owl.carousel.min.css">
 <?
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
-
 ?>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="/local/templates/furniture_pale-blue/components/bitrix/catalog.section.list/catalog_main_sections/owlcarousel/owl.carousel.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="/local/include/owlcarousel/owl.carousel.min.js"></script>
 <?php
-$APPLICATION->IncludeComponent(
-    'bitrix:catalog.section.list', 'catalog_main_sections',
-    [
-        'VIEW_MODE'             => 'TEXT',
-        'SHOW_PARENT_NAME'      => 'Y',
-        'IBLOCK_TYPE'           => 'catalog',
-        'IBLOCK_ID'             => 6,
-        'SECTION_ID'            => $_REQUEST['SECTION_ID'],
-        'SECTION_CODE'          => '',
-        'SECTION_URL'           => '',
-        'COUNT_ELEMENTS'        => 'Y',
-        'TOP_DEPTH'             =>  2,
-        'SECTION_USER_FIELDS'   => [
-            0 => '',
-            1 => '',
-        ],
-        'ADD_SECTIONS_CHAIN'    => 'Y',
-        'CACHE_TYPE'            => 'Y',
-        'CACHE_TIME'            => '36000000',
-        'CACHE_NOTES'           => '',
-        'CACHE_GROUPS'          => 'Y',
-        'COUNT_ELEMENTS_FILTER' => 'CNT_ACTIVE',
-        'SECTION_FIELDS'        => [
-            0 => '',
-            1 => '',
-        ],
-        'FILTER_NAME'           => 'sectionsFilter',
-        'CACHE_FILTER'          => 'N',
-    ],
-    false
-);
+        require $_SERVER['DOCUMENT_ROOT'] . '/local/include/catalog_main_sections.php'
 ?>
 
 
-    <p>
-        Наша компания существует на Российском рынке с 1993 года. За это время «Мебельная компания» прошла большой путь
-        от
-        маленькой торговой фирмы до одного из крупнейших производителей корпусной мебели в России.
-    </p>
-    <p>
-        «Мебельная компания» осуществляет производство мебели на высококлассном оборудовании с применением минимальной
-        доли
-        ручного труда, что позволяет обеспечить высокое качество нашей продукции. Налажен производственный процесс как
-        массового и индивидуального характера, что с одной стороны позволяет обеспечить постоянную номенклатуру изделий
-        и
-        индивидуальный подход – с другой.
-    </p>
-    <h3>Наша продукция</h3>
+<p>
+    Наша компания существует на Российском рынке с 1993 года. За это время «Мебельная компания» прошла большой путь
+    от
+    маленькой торговой фирмы до одного из крупнейших производителей корпусной мебели в России.
+</p>
+<p>
+    «Мебельная компания» осуществляет производство мебели на высококлассном оборудовании с применением минимальной
+    доли
+    ручного труда, что позволяет обеспечить высокое качество нашей продукции. Налажен производственный процесс как
+    массового и индивидуального характера, что с одной стороны позволяет обеспечить постоянную номенклатуру изделий
+    и
+    индивидуальный подход – с другой.
+</p>
+<h3>Наша продукция</h3>
 
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/local/include/articles_list.php';
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php'); ?>
---
+++ local/templates/furniture_pale-blue/components/bitrix/catalog.section.list/catalog_main_sections/script_owlslider.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:       true,
+        margin:     10,
+        items:      4,
+        nav:        true,
+        responsive: {
+            0:    {
+                items: 1
+            },
+            600:  {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        },
+        navClass:   ['owl-prev-local', 'owl-next-local'],
+        center:     true,
+        autoWidth:  true,
+    });
+});
