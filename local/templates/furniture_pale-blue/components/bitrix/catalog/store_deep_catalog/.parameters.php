88b48be35d2f23d7856ed7dc64eb47b72a9a8376
d.kosirev <tolkinv8@gmail.com>
2023-05-05 18:02:17 +0300

    106778 | рефакторинг кода комплексного компонента

--- local/templates/furniture_pale-blue/components/bitrix/catalog/store_deep_catalog/.parameters.php
+++ local/templates/furniture_pale-blue/components/bitrix/catalog/store_deep_catalog/.parameters.php
@@ -1,22 +1,22 @@
 <?php
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
 $arTemplateParameters = [
-    "DETAIL_SHOW_PICTURE"      => [
-        "NAME"     => GetMessage("SHOW_PICTURE_DETAIL"),
-        "TYPE"     => "CHECKBOX",
-        "MULTIPLE" => "N",
-        "DEFAULT"  => "Y",
-        "PARENT"   => 'DETAIL_SETTINGS',
+    'DETAIL_SHOW_PICTURE'      => [
+        'NAME'     => GetMessage('SHOW_PICTURE_DETAIL'),
+        'TYPE'     => 'CHECKBOX',
+        'MULTIPLE' => 'N',
+        'DEFAULT'  => 'Y',
+        'PARENT'   => 'DETAIL_SETTINGS',
     ],
-    "SECTION_SHOW_PARENT_NAME" => [
-        "NAME"     => GetMessage("SHOW_PARENT_NAME"),
-        "TYPE"     => "CHECKBOX",
-        "MULTIPLE" => "N",
-        "DEFAULT"  => "N",
-        "PARENT"   => 'ADDITIONAL_SETTINGS',
+    'SECTION_SHOW_PARENT_NAME' => [
+        'NAME'     => GetMessage('SHOW_PARENT_NAME'),
+        'TYPE'     => 'CHECKBOX',
+        'MULTIPLE' => 'N',
+        'DEFAULT'  => 'N',
+        'PARENT'   => 'ADDITIONAL_SETTINGS',
     ],
 ];
 ?>
